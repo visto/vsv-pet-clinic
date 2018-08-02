15d1e4ddb7fea74a111c9bab8e412b2230b4197f
VSV <viktor.stoitschev@westernacher.com>
2018-08-02 17:43:04 +0200

    Initial Project Add

---
+++ src/main/java/de/stoitschev/vsvpetclinic/VsvPetClinicApplication.java
@@ -0,0 +1,12 @@
+package de.stoitschev.vsvpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsvPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsvPetClinicApplication.class, args);
+	}
+}
